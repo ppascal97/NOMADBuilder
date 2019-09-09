7075a6c37e2b3b482360410bd632d70ae0bd0d6c
ppascal97 <49170339+ppascal97@users.noreply.github.com>
2019-09-09 16:44:47 -0400

    focus on MacOS

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -41,11 +41,11 @@ exit
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = [
-    Linux(:i686, libc=:glibc),
-    Linux(:x86_64, libc=:glibc),
-    Linux(:aarch64, libc=:glibc),
-    Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
-    Linux(:powerpc64le, libc=:glibc),
+    #Linux(:i686, libc=:glibc),
+    #Linux(:x86_64, libc=:glibc),
+    #Linux(:aarch64, libc=:glibc),
+    #Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
+    #Linux(:powerpc64le, libc=:glibc),
     MacOS(:x86_64)
 ]
 
